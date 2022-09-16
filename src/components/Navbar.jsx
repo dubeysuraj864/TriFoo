ea2ff9c867683efc8903e619c1e0b2d929c3e29e
Suraj Dubey <dubeysuraj864@gmail.com>
2022-09-16 22:14:40 +0530

    media query added for phone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,31 @@
 import { Link, Outlet } from "react-router-dom";
 import "../styles/Navbar.css";
-import {MdLocalOffer} from "react-icons/md";
+import { MdLocalOffer } from "react-icons/md";
 function Navbar() {
   return (
     <>
       <h6>
-        <MdLocalOffer /> &nbsp;
-        Free Home Delivery on Order Above $500/-
+        <MdLocalOffer /> &nbsp; Free Home Delivery on Order Above $500/-
       </h6>
-      <nav>
-        <ul>
-          <Link to="/">
-            <span className="logo">TriFoo</span>
-          </Link>
-        </ul>
 
-        <ul>
+      <nav>
+      <ul className="menu">
+        <input type="checkbox" id="toggle" />
+        <label htmlFor="toggle" id="turn">
+          {" "}
+          <span className="burger">
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          
+          </span>
+      
+        </label>
+        <span className="logo">TriFoo</span>
+
+        
+
+        <ul className="navlinks">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -27,11 +37,14 @@ function Navbar() {
             <Link to="/contact">Contact</Link>
           </li>
           <li>
-            <Link to="/tnc">Terms & Conditions</Link>
+            <Link className="tnc-link" to="/tnc">
+              Terms & Conditions
+            </Link>
           </li>
         </ul>
-        <ul>
-        <li>
+        </ul>
+        <ul className="login-cart">
+          <li>
             <Link to="/login">Login</Link>
           </li>
           <li>
